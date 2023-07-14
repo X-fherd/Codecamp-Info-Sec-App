3e823468671fd8e85aaa1578a9972a8dc1173cac
X-fherd <x.fherdinand@gmail.com>
2023-07-13 19:37:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Information Security with HelmetJS
+
+This is the application I made to follow along with [Free Code Camp Information Security Tutorial] https://www.freecodecamp.org/learn/information-security/information-security-with-helmetjs/
+
+*X-fherd*
---
+++ myApp.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet')
+const PORT = process.env.PORT || 3030;
+
+// My Code
+app.listen(PORT, () => {
+  console.log('X-fherd freecodecamp Info-Sec Application started on port ${PORT}');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
